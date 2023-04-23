d9c64bad67ff6bbbc742c99ea1c98618d1512dbe
MokiD0ki <Yermukhamed_Islam@proton.me>
2023-04-23 10:17:36 +0100

    minor change in setttings 2

--- mokash_invasion/settings.py
+++ mokash_invasion/settings.py
@@ -4,7 +4,7 @@ import pygame
 class Settings:
     def __init__(self):
         # Screen settings
-        self.screen_width = 1200
+        self.screen_width = 1400
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
