48331f4f1674f14bab46d10a6c44b03b41e3ca8e
MokiD0ki <Yermukhamed_Islam@proton.me>
2023-04-23 10:16:16 +0100

    minor change in settings

--- mokash_invasion/settings.py
+++ mokash_invasion/settings.py
@@ -4,7 +4,7 @@ import pygame
 class Settings:
     def __init__(self):
         # Screen settings
-        self.screen_width = 1400
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
@@ -19,7 +19,7 @@ class Settings:
         self.bullet_height = 15
         self.bullet_speed = 0.7
         self.bullet_color = (60, 60, 60)
-        self.bullet_max = 3
+        self.bullet_max = 4
 
         # Alien settings
         self.aliens_speed = 0.5
